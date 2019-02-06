9f4140e8f2b0b1bb10ea4cbb5cee1da834e12705
Daniel Zhuang <danielzhuang11@hotmail.com>
2019-02-06 16:52:10 -0600

    bug fixes and formatting

--- Assets/Scripts/Questions.cs
+++ Assets/Scripts/Questions.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
 using System;
+using UnityEditor;
 
 public class Questions : MonoBehaviour {
     public Text question;
 
     public string newQuestion()
     {
+        
+        string[] choices;
+        string[] delimitors = {"  "};
         //loads question and correct answer from text file
         var api = Resources.Load("test") as TextAsset;
         var apiKey = api.text;
+        AssetDatabase.Refresh();
         string[] lines = apiKey.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
         int random = 1;
         while (random % 2 != 0)
@@ -20,8 +25,15 @@ public class Questions : MonoBehaviour {
             random = UnityEngine.Random.Range(0, lines.Length);
         }
 
+        choices = lines[random].Split('+');
 
-        question.text = lines[random];
+        Debug.Log(lines[random]);
+        foreach(String str in choices)
+        {
+             Debug.Log(str);
+        }
+       
+        question.text = (choices[0] + "\n" + choices[1] + "\n" + choices[2] + "\n" + choices[3] + "\n" + choices[4]);
 
         return (lines[random + 1]);
 
